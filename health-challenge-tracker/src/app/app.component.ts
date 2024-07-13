1c6753c06d6daa853cfcb05b99c28ff1a151cf0d
abhishek singh mahra <100080397+Namechangedd@users.noreply.github.com>
2024-07-14 00:36:12 +0530

    color changes

--- health-challenge-tracker/src/app/app.component.ts
+++ health-challenge-tracker/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { SearchfilterComponent } from './components/searchfilter/searchfilter.component';
 import { WorkoutformComponent } from './components/workoutform/workoutform.component';
 import { ProgresschartComponent } from './components/progresschart/progresschart.component';
@@ -9,7 +8,7 @@ import { ProgresschartComponent } from './components/progresschart/progresschart
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,SidebarComponent,SearchfilterComponent,WorkoutformComponent,ProgresschartComponent],
+  imports: [RouterOutlet,HeaderComponent,SearchfilterComponent,WorkoutformComponent,ProgresschartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
