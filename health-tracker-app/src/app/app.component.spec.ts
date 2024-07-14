a2dba561af03b8769b99cf30b2af3eda476a9c07
abhishek singh mahra <100080397+Namechangedd@users.noreply.github.com>
2024-07-14 20:09:39 +0530

    initial commit

--- health-tracker-app/src/app/app.component.spec.ts
+++ health-tracker-app/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'health-challenge-tracker' title`, () => {
+  it(`should have as title 'health-tracker-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('health-challenge-tracker');
+    expect(app.title).toEqual('health-tracker-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, health-challenge-tracker');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('health-tracker-app app is running!');
   });
 });
